af0b64f840708298b33312b4e3d15507c82316a0
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-11 18:00:39 +0900

    Create muzzle fire and hit fire

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class FireController : MonoBehaviour {
 
+	public GameObject fire;
+	public GameObject muzzle;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+
 		if (Input.GetMouseButtonDown (0)) {
 			Ray ray = new Ray (transform.position + new Vector3(0, 0.1f, 0), transform.forward);
 			RaycastHit hit = new RaycastHit ();
+			GameObject muzzleFire = (GameObject)Instantiate (fire, muzzle.transform.position, muzzle.transform.rotation);
+			Destroy (muzzleFire, 0.1f);
 
-
+			if (Physics.Raycast (ray, out hit)) {
+				GameObject hitFire = (GameObject)Instantiate (fire, hit.point, hit.transform.rotation);
+				Destroy (hitFire, 0.1f);
+			}
 		}
 
 				
