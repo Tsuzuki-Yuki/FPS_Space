852b476c60e0cc0fbb5b92d469390a4f30570a9f
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-12 22:42:10 +0900

    Create HeadMarker

--- Assets/Scripts/targetController.cs
+++ Assets/Scripts/targetController.cs
@@ -7,7 +7,7 @@ public class targetController : MonoBehaviour {
 	[SerializeField] private GameObject target;
 	// Use this for initialization
 	void Start () {
-		GameObject targetObj = (GameObject) Instantiate (target, transform.position, Quaternion.identity);
+		GameObject targetObj = (GameObject) Instantiate (target, target.transform.position, Quaternion.identity);
 		targetObj.transform.parent = this.transform;
 		
 	}
