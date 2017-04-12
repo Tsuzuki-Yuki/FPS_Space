0b01d07e6a713fa18ca104e5a46a80cb5f20d660
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-12 17:47:47 +0900

    Locate target

---
+++ Assets/Scripts/targetController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetController : MonoBehaviour {
+
+	[SerializeField] private GameObject target;
+	// Use this for initialization
+	void Start () {
+		GameObject targetObj = (GameObject) Instantiate (target, transform.position, Quaternion.identity);
+		targetObj.transform.parent = this.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
