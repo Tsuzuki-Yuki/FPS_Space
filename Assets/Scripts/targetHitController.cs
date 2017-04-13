8cd134b8a382e971fb8114eb7e0a797e579ba856
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-13 14:04:18 +0900

    Change score system, from TargetHitController to ScoreController

--- Assets/Scripts/targetHitController.cs
+++ Assets/Scripts/targetHitController.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class TargetHitController : MonoBehaviour {
 
-	[SerializeField] private GameObject headMarker;
 	private GameObject target;
 	int targetLife;
-	float scoreDistance;
-	int score;
 	Animator anim;
-	Vector3 headMarkerCenter;
 
 	// Use this for initialization
 	void Start () {
@@ -22,16 +18,12 @@ public class TargetHitController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		headMarkerCenter = headMarker.GetComponent<Renderer>().bounds.center;
+		
 	}
 
 	void OnTriggerEnter (Collider other){
 		if (other.gameObject.tag == "Bullet") {
 			targetLife--;
-			scoreDistance = Vector3.Distance (headMarkerCenter, FireController.hitLocation);
-			score = (int)((1 - scoreDistance) * 100);
-			print (score);
-
 			if (targetLife == 0) {
 				anim.SetBool("broken", true);
 				Invoke ("rebornTarget", 10.0f);
