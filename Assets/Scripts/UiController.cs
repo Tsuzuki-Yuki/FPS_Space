dc0dac5fca1a3e1e547af935adc3ee127538164e
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-13 19:16:57 +0900

    Display score, bullet, bullet box

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour {
+
+	public GameObject canvas;
+	public Text scorePt;
+	public Text bulletNum;
+	public Text bulletBoxNum;
+	// Use this for initialization
+	void Start () {
+		//Instantiate (canvas);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scorePt.text = "Pt : " + ScoreController.totalScore;
+		bulletNum.text = "Bullet : " + FireController.bullet;
+		bulletBoxNum.text = "BulletBox : " + FireController.bulletBox;
+	}
+}
