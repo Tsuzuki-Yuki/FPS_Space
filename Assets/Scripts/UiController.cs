14b5732114d51afd286d49b7575bc9ffa9ecdd59
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-13 19:33:34 +0900

    Display timer

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class UiController : MonoBehaviour {
 
-	public GameObject canvas;
-	public Text scorePt;
-	public Text bulletNum;
-	public Text bulletBoxNum;
+	[SerializeField] private Text Timer;
+	[SerializeField] private Text scorePt;
+	[SerializeField] private Text bulletNum;
+	[SerializeField] private Text bulletBoxNum;
+	float startTime;
+	float limitTime;
 	// Use this for initialization
 	void Start () {
-		//Instantiate (canvas);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		startTime += Time.deltaTime;
+		limitTime = 90.0f - startTime;
+		Timer.text = "タイマー : " + limitTime.ToString("N1") + "s";
 		scorePt.text = "Pt : " + ScoreController.totalScore;
-		bulletNum.text = "Bullet : " + FireController.bullet;
+		bulletNum.text = "Bullet : " + FireController.bullet + "/30";
 		bulletBoxNum.text = "BulletBox : " + FireController.bulletBox;
 	}
 }
