511ace0db6c7869e4e341f89afcae0d12eef0526
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-04-14 09:42:39 +0900

    Zoom up camera, when right click push

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	Camera fpcCamera;
+	bool snipeSwitch;
+	// Use this for initialization
+	void Start () {
+		fpcCamera = GetComponent<Camera> ();
+		snipeSwitch = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+			if (!snipeSwitch) {
+				fpcCamera.fieldOfView = 20;
+				snipeSwitch = true;
+			} else {
+				fpcCamera.fieldOfView = 60;
+				snipeSwitch = false;
+			}
+		}
+	}
+}
